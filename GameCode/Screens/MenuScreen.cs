3e3136d8c3d59152a0acfd2f3d2ab88fcb722b32
MegaChuck64 <cjscottcarey@yahoo.com>
2023-01-16 16:46:58 -0500

    Removed old unused screens

--- GameCode/Screens/MenuScreen.cs
+++ GameCode/Screens/MenuScreen.cs
@@ -10,49 +10,19 @@ namespace GameCode.Screens;
 
 public class MenuScreen : GameScreen
 {
-    Button playButton;
-    Button demoButton;
     Button orthButton;
 
-    OrthographicCamera camera;
     MainGame game => Game as MainGame;
     FPSCounter fpsCounter;
     public MenuScreen(MainGame game) : base(game)
     {
-        camera = new OrthographicCamera(GraphicsDevice);
+
     }
 
     public override void LoadContent()
     {
         base.LoadContent();
         var font = Game.Content.Load<SpriteFont>(@"Fonts\consolas_22");
-        playButton = new Button(Game as MainGame)
-        {
-            Color = Color.Red,
-            HighlightColor = Color.Blue,
-            TextColor = Color.Green,
-            HighlightTextColor = Color.Blue,
-            Filled = false,
-            Font = font,
-            Text = "Play",
-            TextScale = 1f,
-            Rect = new Rectangle(Game.GraphicsDevice.Viewport.Width/2-50, 100, 100, 40),
-            TextOffset = new Point(16,6),            
-        };
-
-        demoButton = new Button(Game as MainGame)
-        {
-            Color = Color.Yellow,
-            HighlightColor = Color.Blue,
-            TextColor = Color.Red,
-            HighlightTextColor = Color.Blue,
-            Filled = false,
-            Font = font,
-            Text = "Demo",
-            TextScale = 1f,
-            Rect = new Rectangle(Game.GraphicsDevice.Viewport.Width / 2 - 50, 148, 100, 40),
-            TextOffset = new Point(16, 6),
-        };
 
         orthButton = new Button(Game as MainGame)
         {
@@ -62,14 +32,12 @@ public class MenuScreen : GameScreen
             HighlightTextColor = Color.Blue,
             Filled = false,
             Font = font,
-            Text = "Orth",
+            Text = "Play",
             TextScale = 1f,
-            Rect = new Rectangle(Game.GraphicsDevice.Viewport.Width / 2 - 50, 196, 100, 40),
+            Rect = new Rectangle(Game.GraphicsDevice.Viewport.Width / 2 - 50, 100, 100, 40),
             TextOffset = new Point(16, 6),
         };
 
-        playButton.OnClick += PlayButton_OnClick;
-        demoButton.OnClick += DemoButton_OnClick;
         orthButton.OnClick += OrthButton_OnClick;
             
 
@@ -84,21 +52,9 @@ public class MenuScreen : GameScreen
         ScreenManager.LoadScreen(new OrthScreen(game), new FadeTransition(GraphicsDevice, Color.Black, 2f));
     }
 
-    private void DemoButton_OnClick(object sender, ClickEventArgs e)
-    {
-        ScreenManager.LoadScreen(new DemoScreen(game), new FadeTransition(GraphicsDevice, Color.Black, 2f));
-    }
-
-    private void PlayButton_OnClick(object sender, ClickEventArgs e)
-    {
-        ScreenManager.LoadScreen(new CrawlScreen(game), new FadeTransition(GraphicsDevice, Color.Black, 2f));
-
-    }
 
     public override void Update(GameTime gameTime)
     {        
-        playButton.Update(gameTime.GetElapsedSeconds());
-        demoButton.Update(gameTime.GetElapsedSeconds());
         orthButton.Update(gameTime.GetElapsedSeconds());
         fpsCounter.Tick(gameTime);
     }
@@ -107,8 +63,6 @@ public class MenuScreen : GameScreen
     public override void Draw(GameTime gameTime)
     {
         game.SpriteBatch.Begin();
-        playButton.Draw(game.SpriteBatch);
-        demoButton.Draw(game.SpriteBatch);
         orthButton.Draw(game.SpriteBatch);
         game.SpriteBatch.DrawCircle(new CircleF(game.MouseState.Position, 4f), 10, Color.Green);
 
